8e11aa32a3b1d3b21f9e03654963eaf7ed345dc3
thiagoaferreira <thiago.aferreira86@gmail.com>
2025-10-16 22:14:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000,
+    host: true
   }
 })
